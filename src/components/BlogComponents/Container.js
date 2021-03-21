f401b3a59392afe6f0d0d4b710ab84c6f0967162
pextech <mcstain1639@gmail.com>
2021-03-21 16:29:24 +0200

    blog finalisation

--- src/components/BlogComponents/Container.js
+++ src/components/BlogComponents/Container.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import Blogs from './Blogs';
+import search from '../../images/search.png';
+import Latest from './Latest';
+import future from '../../images/future.jpg';
+import Todo from '../../images/Todo.jpg';
+import Avatar from '../../images/Avatar.png';
+import Code from '../../images/Code.png';
 
 function Container() {
+  const title1 = 'Test: Virtual Coffee Chat';
+  const title2 = 'Test: CS vs CIS: JMU Grad Panel';
+  const title3 = 'Test: FAST Enterprises: Technical interview Workshop';
+  const title4 = 'Test: JMU CISE Student Diversity Council & Mentorship Program';
+  const description = 'Test: When someone shows you who they are, believe them believe them believe them';
+
   return (
-    <div className="bg-primary-200 h-screen">
+    <div className="bg-primary-200 flex flex-row p-20 justify-center section-container">
       {/* main section */}
-      <div>
+      <div className="flex flex-row flex-wrap w-full blog-container">
         {/* blog containers */}
+        <Blogs image={future} title={title1} description={description} date="March 19, 2020" />
+        <Blogs image={Code} title={title2} description={description} date="March 12, 2021" />
+        <Blogs image={Avatar} title={title3} description={description} date="March 18, 2021" />
+        <Blogs image={Todo} title={title4} description={description} date="January 13, 2020" />
       </div>
-      <div>
+      <div className="p-10 w-5/12 blog-side">
         {/* left side */}
+        <div className="absolute w-1/5 search">
+          <input type="text" placeholder="Search..." className="w-full  h-12 search rounded-md pl-10 inp text-gray-200" />
+          <button type="button"><img src={search} alt="search" className="w-6 h-6 absolute right-2 top-3" /></button>
+        </div>
+        <div className="mainDiv mt-28">
+          <p className="text-xl text-primary-100">Latest blogs</p>
+          <div className="border-b-2 my-2 border-primary-100 w-20" />
+        </div>
+        <Latest image={future} title={title1} date="March 19, 2020" />
+        <Latest image={Code} title={title2} date="March 12, 2021" />
+        <Latest image={Avatar} title={title3} date="March 18, 2021" />
+        <Latest image={Todo} title={title4} date="January 13, 2020" />
+
       </div>
     </div>
 
---
+++ src/components/BlogComponents/Latest.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+function Latest({
+  title, image, date,
+}) {
+  return (
+    <div className="latest__container flex flex-row my-10">
+      <img alt="latest" src={image} className=" h-28 rounded-md" width="100" />
+      <div className="flex flex-col">
+        <div className="date flex flex-row ml-5 text-primary-100">
+          <svg className="w-6 h-6 font-light " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <p className="ml-4 font-light text-sm mt-1">{date}</p>
+        </div>
+        <div className="title ml-10 mt-4 font-light text-gray-200 w-10/12">
+          <p>{title}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Latest;
