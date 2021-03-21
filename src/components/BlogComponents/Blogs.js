2829a171cd87926ba19b8d121b87a8713f7994f5
pextech <mcstain1639@gmail.com>
2021-03-21 16:43:58 +0200

    blog finalisation

--- src/components/BlogComponents/Blogs.js
+++ src/components/BlogComponents/Blogs.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Blogs({
   title, image, description, date,
@@ -7,21 +8,21 @@ function Blogs({
   return (
     <div className="blogs flex flex-col w-5/12 bg-gray-100 rounded-md m-10">
       {/* blog section */}
-      <img src={image} alt="future" className="w-full h-full rounded-md rounded-b-none" />
+      <img src={image} alt="future" className="w-full h-full rounded-md rounded-b-none " />
       <div className="date flex flex-row mt-4 ml-5">
         <svg className="w-6 h-6 font-light" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         <p className="ml-4 font-light text-sm mt-1">{date}</p>
       </div>
       <div className="title ml-10 mt-4 font-semibold w-10/12 text-gray-700">
-        <p>{title}</p>
+        <Link to="/blog"><p className="font-medium hover:font-bold">{title}</p></Link>
       </div>
-      <div className="description ml-10 my-6 font-light text-gray-700">
-        <p>
+      <div className="description ml-10 my-6 ">
+        <p className="font-light hover:font-normal text-gray-700">
           {description}
         </p>
       </div>
       <div className="readmore ml-10 mb-10">
-        <button type="button" className="border border-gray-700 w-40 h-12 shadow-md rounded-md">Read More</button>
+        <button type="button" className="border border-gray-700 hover:bg-gray-700 hover:text-gray-200 w-40 h-12 shadow-md rounded-md hover:shadow-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ...">Read More</button>
       </div>
     </div>
   );
--- src/components/BlogComponents/Latest.js
+++ src/components/BlogComponents/Latest.js
@@ -6,7 +6,7 @@ function Latest({
 }) {
   return (
     <div className="latest__container flex flex-row my-10">
-      <img alt="latest" src={image} className=" h-28 rounded-md" width="100" />
+      <img alt="latest" src={image} className=" h-28 rounded-md hover:shadow-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ..." width="100" />
       <div className="flex flex-col">
         <div className="date flex flex-row ml-5 text-primary-100">
           <svg className="w-6 h-6 font-light " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
